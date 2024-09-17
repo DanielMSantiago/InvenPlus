08746afcca4c8bbab5670a74552bd33af2ed33d0
B31rut <daniel.santiago0302@gmail.com>
2024-09-16 22:16:39 -0400

    Finished invoice Schema in Server/models/Invoice.jsx

--- Server/models/invoice.jsx
+++ Server/models/invoice.jsx
@@ -1,4 +1,47 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const InvoiceSchema = new Schema({});
+const InvoiceSchema = new Schema({
+  poNum: {
+    type: String,
+    required: [true, "PO Number Needed"],
+  },
+  invoiceNum: {
+    type: String,
+    required: [true, "Distributor Invoice Needed"],
+  },
+  distributor: {
+    type: String,
+    required: [true, "Distributor Name Needed"],
+  },
+  branch: {
+    type: String,
+    required: [true, "Branch Location Needed"],
+  },
+  customerName: {
+    type: String,
+  },
+  warranty: {
+    type: Boolean,
+    required: [true],
+  },
+  amount: {
+    type: Number,
+    required: [true, "Amount Ordered Needed"],
+  },
+  itemName: {
+    type: String,
+    required: [true, "Item Description Needed"],
+  },
+  modelNum: {
+    type: String,
+    required: [true, "Model Number Needed"],
+  },
+  serialNum: {
+    type: String,
+  },
+  price: {
+    type: String,
+    required: [true, "Price Needed"],
+  },
+});
