2089e82b9334e9f61aab3246cd3434c075137f66
B31rut <daniel.santiago0302@gmail.com>
2024-09-30 21:36:15 -0400

    Found my form was had a misplaced closing form tag that was inserted before the form rows. Added the closing tag after the form rows data. Working on Server.js to display message once form data/rows are sent to the backend

--- Server/models/invoice.jsx
+++ Server/models/invoice.jsx
@@ -1,51 +1,25 @@
+// models/invoice.js
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const InvoiceSchema = new Schema({
-  poNum: {
-    type: String,
-    required: [true, "PO Number Needed"],
-  },
-  invoiceNum: {
-    type: String,
-    required: [true, "Distributor Invoice Needed"],
-  },
-  distributor: {
-    type: String,
-    required: [true, "Distributor Name Needed"],
-  },
-  branch: {
-    type: String,
-    required: [true, "Branch Location Needed"],
-  },
-  customerName: {
-    type: String,
-  },
-  warranty: {
-    type: Boolean,
-    required: [true],
-  },
-  amount: {
-    type: Number,
-    required: [true, "Amount Ordered Needed"],
-  },
-  itemName: {
-    type: String,
-    required: [true, "Item Description Needed"],
-  },
-  modelNum: {
-    type: String,
-    required: [true, "Model Number Needed"],
-  },
-  serialNum: {
-    type: String,
-  },
-  price: {
-    type: String,
-    required: [true, "Price Needed"],
-  },
+const InvoiceSchema = new mongoose.Schema({
+  poNum: String,
+  invoiceNum: String,
+  distributor: String,
+  branch: String,
+  customerName: String,
+  warranty: String,
+  items: [
+    {
+      amount: String,
+      itemName: String,
+      modelNum: String,
+      serialNum: String,
+      price: String,
+      received: Boolean,
+    },
+  ],
 });
 
-const InvoiceModel = mongoose.model("invoice", InvoiceSchema);
+const InvoiceModel = mongoose.model("Invoice", InvoiceSchema);
 
 module.exports = InvoiceModel;
