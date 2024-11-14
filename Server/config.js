9b6137bbd2345055a205c38378504f177f9bd68e
B31rut <daniel.santiago0302@gmail.com>
2024-11-13 21:53:57 -0500

    Added correct connection string for mondoDB in config.js. Added connection to mongoDB in index.js using variable containing connection string that was imported from config.js. Bash terminal in Bash terminal provided successful connection to database

---
+++ Server/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://111-aZ:Ranger94@cluster1.y8fehmu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1'
---
+++ Server/models/invoiceSchema.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const invoiceSchema = mongoose.Schema({
+    PoNumber: {
+        type: Number,
+        required: true,
+    },
+    DistroInvoiceNum: {
+        type: Number,
+        required: true,
+    },
+    Distro: {
+        type: String,
+        required: true,
+    },
+    DistroBranch: {
+        type: String,
+        required: true,
+    },
+    CustName: {
+        type: String,
+        required: true,
+    },
+    Warranty: {
+        type: Boolean,
+        required: true,
+    },
+    AmountOrd: {
+        type: Number,
+        required: true,
+    },
+    ItemName: {
+        type: String,
+        required: true,
+    },
+    ItemModel: {
+        type: String,
+        required: true,
+    },
+    ItemSerial: {
+        type: String,
+        required: true,
+    },
+    ItemPrice: {
+        type: Number,
+        required: true,
+    }
+
+})
+
+export const Invoice = mongoose.model(invoiceSchema)
