df73a9a90d7853441461dc1562ee362394cfd9df
B31rut <daniel.santiago0302@gmail.com>
2024-12-08 10:31:34 -0500

    Moved http request methods to a new invoiceRoutes file. Updated invoiceRoutes with express to use express.Router for my http methods. Added get method for searching invoice by PO number. Delete method for deleting invoice by PO number. Added middleware to handle CORS policy.

--- Server/index.js
+++ Server/index.js
@@ -2,102 +2,27 @@ import express, { response } from "express";
 import { mongoDBURL, PORT } from "./config.js";
 import mongoose from "mongoose";
 import { Invoice } from "./models/invoiceSchema.js";
+import invoiceRoutes from "./routes/invoiceRoutes.js"
+import cors from "cors";
+
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+//Middleware for handling CORS policy
+app.use(cors({
+    origin: "http://localhost:5555",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-type']
+}));
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Hello');
 });
 
-app.post('/invoice', async (request, response) => {
-    try {
-        if (!request.body.PoNumber ||
-            !request.body.DistroInvoiceNum ||
-            !request.body.Distro ||
-            !request.body.DistroBranch ||
-            !request.body.Warranty ||
-            !request.body.AmountOrd ||
-            !request.body.ItemModel ||
-            !request.body.ItemName ||
-            !request.body.ItemPrice
-        ) {
-            return response.status(400).send({
-                message: "Please send all required field: Po Number, Invoice Number, Distributor, Distributor Branch, Warranty, Amount Order, Item Name, Item Model Number",
-            })
-        }
-        const newInvoice = {
-            PoNumber: request.body.PoNumber,
-            DistroInvoiceNum: request.body.DistroInvoiceNum,
-            Distro: request.body.Distro,
-            DistroBranch: request.body.DistroBranch,
-            CustName: request.body.CustName,
-            Warranty: request.body.Warranty,
-            AmountOrd: request.body.AmountOrd,
-            ItemName: request.body.ItemName,
-            ItemModel: request.body.ItemModel,
-            ItemSerial: request.body.ItemSerial,
-            ItemPrice: request.body.ItemPrice
-        }
 
-        const invoice = await Invoice.create(newInvoice)
-
-        return response.status(201).send(invoice)
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ mesage: 'Internal Server Error' });
-    }
-});
-
-app.get('/invoice', async (request, response) => {
-    try {
-
-        const invoices = await Invoice.find({});
-        return response.status(200).json(invoices)
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-app.get('/invoice/:PoNumber', async (request, response) => {
-    try {
-
-        const { PoNumber } = request.params
-        const invoices = await Invoice.findOne({ PoNumber })
-
-        return response.status(200).json(invoices)
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-app.put('/invoice/:PoNumber', async (request, response) => {
-    try {
-        if (!request.body.PoNumber ||
-            !request.body.DistroInvoiceNum ||
-        ) {
-            return response.status(400).send({
-                message: "Send all required fields: PoNumber, Distributor Invoice Number"
-            });
-        }
-        const { PoNumber } = request.params;
-        const result = await Invoice.findOneAndUpdate({ PoNumber });
-
-        if (!result) {
-            return response.status(404).json({ message: "Invoice Not Found" })
-        }
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-})
 
 mongoose.connect(mongoDBURL).then(() => {
     console.log('App connected to database')
---
+++ Server/routes/invoiceRoutes.js
@@ -0,0 +1,92 @@
+import express from 'express';
+import { Invoice } from '../models/invoiceSchema';
+
+const router = express.Router();
+
+//Send an entered invoice to the database
+router.post('/', async (request, response) => {
+    try {
+        if (!request.body.PoNumber ||
+            !request.body.DistroInvoiceNum ||
+            !request.body.Distro ||
+            !request.body.DistroBranch ||
+            !request.body.Warranty ||
+            !request.body.AmountOrd ||
+            !request.body.ItemModel ||
+            !request.body.ItemName ||
+            !request.body.ItemPrice
+        ) {
+            return response.status(400).send({
+                message: "Please send all required field: Po Number, Invoice Number, Distributor, Distributor Branch, Warranty, Amount Order, Item Name, Item Model Number",
+            })
+        }
+        const newInvoice = {
+            PoNumber: request.body.PoNumber,
+            DistroInvoiceNum: request.body.DistroInvoiceNum,
+            Distro: request.body.Distro,
+            DistroBranch: request.body.DistroBranch,
+            CustName: request.body.CustName,
+            Warranty: request.body.Warranty,
+            AmountOrd: request.body.AmountOrd,
+            ItemName: request.body.ItemName,
+            ItemModel: request.body.ItemModel,
+            ItemSerial: request.body.ItemSerial,
+            ItemPrice: request.body.ItemPrice
+        }
+
+        const invoice = await Invoice.create(newInvoice)
+
+        return response.status(201).send(invoice)
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ mesage: 'Internal Server Error' });
+    }
+});
+
+//get method for finding invoices
+router.get('/', async (request, response) => {
+    try {
+
+        const invoices = await Invoice.find({});
+        return response.status(200).json(invoices)
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//get method for finding invoice by PO Number
+router.get('/:PoNumber', async (request, response) => {
+    try {
+
+        const { PoNumber } = request.params
+        const invoices = await Invoice.findOne({ PoNumber })
+
+        return response.status(200).json(invoices)
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//Delete an invoice using PO Number
+router.delete('/:PoNumber', async (request, response) => {
+    try {
+        const { PoNumber } = request.params;
+
+        const result = await Invoice.findOneAndDelete({ PoNumber })
+
+        if (!result) {
+            return response.status(404).json({ message: "Invoice not found" })
+        }
+
+        return response.status(200).send({ message: 'Invoice deleted successfully' })
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message })
+    }
+})
+
+export default router
